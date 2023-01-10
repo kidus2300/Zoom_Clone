d47443289c1fc572d47623d7114ee5b250b0fcea
kidus2300 <90652189+kidus2300@users.noreply.github.com>
2023-01-09 22:15:56 -0500

    able to see others video

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
-const express = require("express")
-const app = express()
-const server = require('http').Server(app)
+const express = require("express");
+const app = express();
+// const cors = require('cors')
+// app.use(cors())
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+const { v4: uuidV4 } = require("uuid");
 
-app.set('view engine','ejs')
+app.use("/peerjs", peerServer);
 
-app.get('/', (req, res) => {
-   res.render('room')
-})
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
 
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
-server.listen(4000)
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+    socket.on("message", (message) => {
+      //send message to the same room
+      io.to(roomId).emit("createMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(4000);
